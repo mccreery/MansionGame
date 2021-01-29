7ad9bf2260cebb841b7573e2cf9fda64e92ff3e8
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2021-01-29 02:02:34 +0000

    Add door open script + child propogate clicks

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public GameObject hinge;
+
+    public float openRotation = 100.0f;
+
+    private bool open;
+
+    private float animationStartTime;
+    public float animationTime = 1.0f;
+
+    private void OnMouseDown() => Toggle();
+
+    public void Toggle()
+    {
+        open = !open;
+        animationStartTime = Time.time;
+    }
+
+    private void Update()
+    {
+        float t = Mathf.Clamp01((Time.time - animationStartTime) / animationTime);
+        t = Mathf.SmoothStep(0.0f, 1.0f, t);
+
+        if (!open)
+        {
+            t = 1.0f - t;
+        }
+
+        hinge.transform.rotation = Quaternion.Slerp(Quaternion.identity, Quaternion.Euler(0, openRotation, 0), t);
+    }
+}
---
+++ Assets/Scripts/Propogate.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class Propogate : MonoBehaviour
+{
+    public UnityEvent onMouseDown;
+    public UnityEvent onMouseUp;
+
+    private void OnMouseDown() => onMouseDown.Invoke();
+    private void OnMouseUp() => onMouseUp.Invoke();
+}
