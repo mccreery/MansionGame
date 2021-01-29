182208c35a40456331bba9e27481a76aa1c42381
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2021-01-29 02:45:30 +0000

    Remove jump from walk script

--- Assets/Scripts/Walk.cs
+++ Assets/Scripts/Walk.cs
@@ -3,12 +3,6 @@
 [RequireComponent(typeof(CharacterController))]
 public class Walk : MonoBehaviour
 {
-    public float jumpVelocity = 1f;
-    public float gravity = 1f;
-
-    public string jumpButton = "Jump";
-    public string crouchButton = "Crouch";
-
     public float walkSpeed = 1.5f;
 
     private CharacterController characterController;
@@ -17,31 +11,11 @@ public class Walk : MonoBehaviour
         characterController = GetComponent<CharacterController>();
     }
 
-    private float yVelocity;
-
     private void Update()
     {
         Vector3 walkDirection = transform.right * Input.GetAxisRaw("Horizontal") + transform.forward * Input.GetAxisRaw("Vertical");
         Vector3 walkVelocity = Vector3.ClampMagnitude(walkDirection, 1) * walkSpeed;
 
-        if (characterController.isGrounded && Input.GetButtonDown(jumpButton))
-        {
-            yVelocity = jumpVelocity;
-        }
-        else if ((characterController.collisionFlags & CollisionFlags.Above) != 0 && yVelocity > 0)
-        {
-            yVelocity = 0;
-        }
-        else
-        {
-            yVelocity -= gravity * Time.deltaTime;
-        }
-
-        characterController.Move((walkVelocity + Vector3.up * yVelocity) * Time.deltaTime);
-    }
-
-    private void OnGUI()
-    {
-        GUI.Label(new Rect(5, 5, 100, 100), characterController.isGrounded.ToString());
+        characterController.SimpleMove(walkVelocity);
     }
 }
