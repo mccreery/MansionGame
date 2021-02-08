bf17ed8d2f423255df8a8013647f93ab6f00426d
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2021-02-08 01:23:53 +0000

    Add pins with string prefab

---
+++ Assets/Scripts/PinPoint.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class PinPoint : MonoBehaviour, IInteractable
+{
+    public string doublePinTag;
+    public string singlePinTag;
+
+    public GameObject singlePinItemPrefab;
+
+    public ItemPickup Interact(ItemPickup heldItem)
+    {
+        if (heldItem.CompareTag(doublePinTag))
+        {
+            GameObject singlePin = Instantiate(singlePinItemPrefab);
+            singlePin.tag = singlePinTag;
+            return singlePin.GetComponent<ItemPickup>();
+        }
+        else if (heldItem.CompareTag(singlePinTag))
+        {
+            return null;
+        }
+        else
+        {
+            return heldItem;
+        }
+    }
+}
