836d98c3ee6b7ea2d892f387a8977842c6269ba1
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2021-02-08 04:01:15 +0000

    Add strings between pairs of pins on map

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -1,13 +1,13 @@
 using System;
 using UnityEngine;
 
-[RequireComponent(typeof(LineRenderer))]
 public class Map : MonoBehaviour
 {
     private PinPoint[] pinnedPoints = new PinPoint[4];
     private int pinCount = 0;
 
     public bool AllPinsPlaced => pinCount == pinnedPoints.Length;
+    public float stringHeight = 0.1f;
 
     // Pins can be pinned multiple times with this method
     // The pinpoints track this themselves
@@ -18,6 +18,7 @@ public class Map : MonoBehaviour
         {
             pinnedPoints[i] = point;
             ++pinCount;
+            UpdateLines();
         }
     }
 
@@ -29,6 +30,38 @@ public class Map : MonoBehaviour
         {
             pinnedPoints[i] = null;
             --pinCount;
+            UpdateLines();
+        }
+    }
+
+    public LineRenderer[] lineRenderers;
+
+    private void Start()
+    {
+        UpdateLines();
+    }
+
+    private void UpdateLines()
+    {
+        for (int i = 0; i < lineRenderers.Length; i++)
+        {
+            PinPoint pointA = pinnedPoints[i * 2];
+            PinPoint pointB = pinnedPoints[i * 2 + 1];
+
+            Vector3 stringOffset = new Vector3(0, stringHeight);
+
+            if (pointA != null && pointB != null)
+            {
+                lineRenderers[i].enabled = true;
+                lineRenderers[i].SetPositions(new Vector3[] {
+                    pointA.transform.position + stringOffset,
+                    pointB.transform.position + stringOffset
+                });
+            }
+            else
+            {
+                lineRenderers[i].enabled = false;
+            }
         }
     }
 }
