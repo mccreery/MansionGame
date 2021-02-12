eb0c99ae72ed245c4359526de2c8887a6792f35c
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2021-02-12 02:50:12 +0000

    Fix record player animations

--- Assets/Scripts/RecordPlayerInteraction.cs
+++ Assets/Scripts/RecordPlayerInteraction.cs
@@ -4,54 +4,52 @@
 [RequireComponent(typeof(Animator))]
 public class RecordPlayerInteraction : MonoBehaviour, IInteractable
 {
-    public ItemPickup correctRecord;
-
     public Message message;
-    public GameObject recordInWorld;
-
     public Cabinet cabinet;
+    public Transform spindle;
 
     private AudioSource audioSource;
     private Animator animator;
+
+    private ItemRecord currentRecord;
 
     public void Start()
     {
         audioSource = GetComponent<AudioSource>();
         animator = GetComponent<Animator>();
-        recordInWorld.SetActive(false);
     }
 
     public void Interact(Hotbar hotbar)
     {
-        if (hotbar.SelectedItem is ItemRecord record)
+        if (currentRecord != null)
         {
-            recordInWorld.SetActive(true);
+            hotbar.Add(currentRecord);
+            currentRecord = null;
+            audioSource.Stop();
+
+            animator.Play("NoMovement");
+        }
+        else if (hotbar.SelectedItem is ItemRecord record)
+        {
+            hotbar.Remove(hotbar.SelectedSlot, true);
+
+            currentRecord = record;
+
+            record.transform.SetParent(spindle);
+            record.transform.localPosition = Vector3.zero;
+            record.transform.localRotation = Quaternion.identity;
+            record.transform.localScale = Vector3.one;
+
             animator.Play(record.recordPlayerAnimationState, -1, 0);
         }
         else
         {
             message.ShowMessage("It's stuck on maximum volume.");
         }
-
-        //if (hotbar.SelectedItem == correctRecord)
-        //{
-        //    recordInWorld.SetActive(true);
-        //    cabinet.Smash();
-        //    hotbar.Remove(hotbar.SelectedSlot, false);
-        //}
-        //else if (hotbar.SelectedItem != null)
-        //{
-        //    message.ShowMessage("That doesn't seem right.");
-        //}
-        //else
-        //{
-        //    message.ShowMessage("Looks like a record could go here.");
-        //}
     }
 
     public void PlayRecord(AudioClip song)
     {
-        audioSource.Stop();
         audioSource.clip = song;
         audioSource.Play();
     }
