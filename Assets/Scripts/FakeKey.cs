c3136946b9cf3d684c64b2eddd9c12c13271e1d7
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2021-02-12 06:17:58 +0000

    Make key drop from ceiling properly

--- Assets/Scripts/FakeKey.cs
+++ Assets/Scripts/FakeKey.cs
@@ -9,13 +9,24 @@ public class FakeKey : MonoBehaviour
     private void Awake()
     {
         rigidbody = GetComponent<Rigidbody>();
+        realKey.SetActive(false);
     }
+
+    private bool startedMoving;
 
     private void FixedUpdate()
     {
-        if (rigidbody.velocity.sqrMagnitude < 0.1f)
+        if (!startedMoving)
         {
-            Instantiate(realKey, transform.localPosition, transform.localRotation, transform.parent);
+            if (rigidbody.velocity.sqrMagnitude > 0.1f)
+            {
+                startedMoving = true;
+            }
+        }
+        else if (rigidbody.velocity.sqrMagnitude < 0.1f)
+        {
+            realKey.SetActive(true);
+            new TransformData(transform).CopyTo(realKey.transform, true);
             Destroy(gameObject);
         }
     }
