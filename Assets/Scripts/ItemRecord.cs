39c49ac9db9564f5ed266a8d39fbbf103af1834b
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2021-02-12 02:07:53 +0000

    Add first playable record

---
+++ Assets/Scripts/ItemRecord.cs
@@ -0,0 +1,4 @@
+public class ItemRecord : ItemPickup
+{
+    public string recordPlayerAnimationState;
+}
--- Assets/Scripts/RecordPlayerInteraction.cs
+++ Assets/Scripts/RecordPlayerInteraction.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
+[RequireComponent(typeof(Animator))]
 public class RecordPlayerInteraction : MonoBehaviour, IInteractable
 {
     public ItemPickup correctRecord;
@@ -9,26 +11,48 @@ public class RecordPlayerInteraction : MonoBehaviour, IInteractable
 
     public Cabinet cabinet;
 
+    private AudioSource audioSource;
+    private Animator animator;
+
     public void Start()
     {
+        audioSource = GetComponent<AudioSource>();
+        animator = GetComponent<Animator>();
         recordInWorld.SetActive(false);
     }
 
     public void Interact(Hotbar hotbar)
     {
-        if (hotbar.SelectedItem == correctRecord)
+        if (hotbar.SelectedItem is ItemRecord record)
         {
             recordInWorld.SetActive(true);
-            cabinet.Smash();
-            hotbar.Remove(hotbar.SelectedSlot, false);
-        }
-        else if (hotbar.SelectedItem != null)
-        {
-            message.ShowMessage("That doesn't seem right.");
+            animator.Play(record.recordPlayerAnimationState, -1, 0);
         }
         else
         {
-            message.ShowMessage("Looks like a record could go here.");
+            message.ShowMessage("It's stuck on maximum volume.");
         }
+
+        //if (hotbar.SelectedItem == correctRecord)
+        //{
+        //    recordInWorld.SetActive(true);
+        //    cabinet.Smash();
+        //    hotbar.Remove(hotbar.SelectedSlot, false);
+        //}
+        //else if (hotbar.SelectedItem != null)
+        //{
+        //    message.ShowMessage("That doesn't seem right.");
+        //}
+        //else
+        //{
+        //    message.ShowMessage("Looks like a record could go here.");
+        //}
+    }
+
+    public void PlayRecord(AudioClip song)
+    {
+        audioSource.Stop();
+        audioSource.clip = song;
+        audioSource.Play();
     }
 }
